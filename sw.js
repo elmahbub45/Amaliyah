7dffa1bdc3c5d4c25225de8fede6255a7a309c9a
elmahbub45 <elmahbub45@gmail.com>
2026-08-25 06:43:25 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='amaliyah-v2-44-1-admin-friendly-mode';
+const C='amaliyah-v2-44-2-quran-mushaf-frame';
 const A=['./','./index.html','./style.css','./app.js','./icon-library.js','./reader.html','./reader.css','./reader.js','./quran.html','./quran.css','./quran.js','./quran-config.js','./books.json','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 
 self.addEventListener('install',e=>{
