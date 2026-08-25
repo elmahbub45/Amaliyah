959cfc0831a5344f3ad33ef4808f6bec78b6cca8
elmahbub45 <elmahbub45@gmail.com>
2026-08-25 09:45:59 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='amaliyah-v2-45-2-library-category-swipe-direction';
+const C='amaliyah-v2-45-3-icon-polish';
 const A=['./','./index.html','./style.css','./app.js','./icon-library.js','./reader.html','./reader.css','./reader.js','./quran.html','./quran.css','./quran.js','./quran-config.js','./books.json','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 
 self.addEventListener('install',e=>{
